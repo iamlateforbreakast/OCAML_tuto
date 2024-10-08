4dbb7d2b16c63a6497bbd6bb9e993dffd9861c7b
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-10-08 12:13:42 +0100

    Correction

--- tuto1.ml
+++ tuto1.ml
@@ -1,10 +1,21 @@
+#use "topfind";;
+#require "base";;
 open Base
-open Stdio
+open Stdlib
 
 (* Single line comment *)
 
+(* A structure *)
+type my_struct = { count : int; step: int}
+
 (** f_start *)
-let rec f_start =
+let rec f_start x =
+  if x.count == 6 then { count = 0; step = 6}
+  else 
+    f_start { count = x.count + 1; step = 1}
+
+let test = { count = 0; step = 1}
 
 let () =
-  printf " " ( f_start );;
+  let t = f_start test
+  Printf.printf ("%d\n", t.count)

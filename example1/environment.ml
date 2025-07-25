cb0569426e6c50875ad70c7048e4d56fc75d6a92
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-25 10:25:32 +0200

    Update environment.ml

--- example1/environment.ml
+++ example1/environment.ml
@@ -1,1 +1,11 @@
-environment.ml
+(* environment.ml *)
+
+type Environment = { temp : float }
+
+(* Constructor *)
+let create () = { temp = 25.0 }
+
+(* Printer *)
+let print (e: Environment) =
+  let _ = Printf.printf "Environment:\n" in
+  let _ = Printf.printf ".temp = %f\n" e.temp

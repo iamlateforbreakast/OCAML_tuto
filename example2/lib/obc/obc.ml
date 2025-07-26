703e09b7642df7f1e810ef6454f448592aaf4f5f
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-26 12:28:46 +0200

    Update obc.ml

---
+++ example2/lib/obc/obc.ml
@@ -0,0 +1,15 @@
+(* obc.ml *)
+
+open Bus
+
+module Obc = struct
+   type t {pc: int}
+         
+   (* Constructor *)
+   let create () = {pc=0}
+
+   (* Printer *)
+
+   (* execute *)
+   let execute t s = 
+end

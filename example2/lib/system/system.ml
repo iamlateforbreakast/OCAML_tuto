28c79f7e7371c1fe85bb1a7461cc2d37e3fe2981
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-27 13:52:40 +0200

    Update system.ml

--- example2/lib/system/system.ml
+++ example2/lib/system/system.ml
@@ -16,10 +16,10 @@ module System = struct
    let create () = {obc=Obc.create();equipment= Equipment.create();bus=Bus.create()}
 
    (* Printer *)
-   let print (s: System.t) =
+   let print (s: t) =
       Printf.printf "System:\n"
 
    (* execute *)
-   let step (t: float) (s: System.t) : System.t = 
+   let step (t: float) (s: t) : System.t = 
       {obc=Obc.step t s.obc; equipment=Equipment.step t s.equipment}
 end

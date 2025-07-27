c284089967a4b45822a1edddb65ded9cd0885e9a
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-27 21:35:51 +0200

    Update bus.ml

--- example2/lib/bus/bus.ml
+++ example2/lib/bus/bus.ml
@@ -10,5 +10,12 @@ module Bus = struct
   let print (b: t) =
     let _ = Printf.printf "Bus:\n" in
     Printf.printf ".temp = %f\n" b.temp
+  
+  (* Step *)
+  let step (delta_t: float)(b: t) : t =
+    if (delta_t > 10.0) then
+      { temp: 30.0 }
+    else
+      { temp: 25.0 }
 
 end

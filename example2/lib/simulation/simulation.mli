d0ea49589e7816c7a52d81e6f24254662e80560d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-28 10:22:43 +0200

    Update simulation.mli

--- example2/lib/simulation/simulation.mli
+++ example2/lib/simulation/simulation.mli
@@ -1,12 +1,30 @@
-sig
+(* simulation.mli *)
+
+(** This module defines the core simulation logic,
+    managing time, quanta, and interacting with
+    System and Environment components. *)
+
+open System
+open Environment
+
+module Simulation : sig
+  (** The internal state of the simulation. *)
   type state =
-  {
-     time: float;
-     quanta: float;
-     env: Environment.t;
-     sys: System.t;
-  }
-  val create -> int
-  val print
-  val step : int -> int
+    {
+      time: float;    (** Current simulation time *)
+      quanta: float;  (** Time step increment per iteration *)
+      env: Environment.t; (** The current environment state *)
+      sys: System.t;  (** The current system state *)
+    }
+
+  (** [create ()] initializes a new simulation state with default values. *)
+  val create : unit -> state
+
+  (** [print s] prints the current state [s] of the simulation to standard output. *)
+  val print : state -> unit
+
+  (** [execute t s] runs the simulation from state [s] for a target duration [t].
+      It returns the final state after the execution. *)
+  val execute : float -> state -> state
+
 end

f8f0ec935d0267c325dd0ae21a9ac58f96598da2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-30 10:19:09 +0200

    Update main.ml

--- example2/bin/main.ml
+++ example2/bin/main.ml
@@ -6,5 +6,5 @@ open Simulation
 let () = 
   let _ = print_endline "Hello, World!"in
   let s0 = Simulation.create() in
-  let s_final = Simulation.execute(100.0, s0) in
+  let s_final = Simulation.execute 100.0 s0 in
   Simulation.print(s_final)

cfd8bbcd259c6200972a90546c30482bfffcaeac
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-07-30 14:45:12 +0200

    Update main.ml

--- example2/bin/main.ml
+++ example2/bin/main.ml
@@ -3,6 +3,16 @@
 open Example2_lib
 open Simulation
 
+let usage_msg = "main -elf file.elf"
+
+let elf_file = ref []
+
+let output_file = ""
+
+let spec_list =
+[("-elf", Arg.set elf_file, "elf file");
+ ("-o", Arg.set_string output_file, "output file")]
+
 let () = 
   let _ = print_endline "Hello, World!"in
   let s0 = Simulation.create() in

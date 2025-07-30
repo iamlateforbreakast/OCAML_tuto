020db7459a4fe625f3a132b4bceaeda5830cffdc
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2025-07-30 20:58:06 +0100

    Update

--- example2/bin/main.ml
+++ example2/bin/main.ml
@@ -5,16 +5,21 @@ open Simulation
 
 let usage_msg = "main -elf file.elf"
 
-let elf_file = ref []
+let elf_file = ref ""
 
-let output_file = ""
+let input_files = ref []
+
+let output_file = ref ""
+
+let anon_func filename = input_files := filename :: !input_files
 
 let spec_list =
-[("-elf", Arg.set_string elf_file, "elf file");
- ("-o", Arg.set_string output_file, "output file")]
+[("-elf", Arg.Set_string elf_file, "elf file");
+ ("-o", Arg.Set_string output_file, "output file")]
 
 let () = 
   let _ = print_endline "Hello, World!"in
+  let _ = Arg.parse spec_list anon_func usage_msg in
   let s0 = Simulation.create() in
   let s_final = Simulation.execute 100.0 s0 in
   Simulation.print(s_final)
